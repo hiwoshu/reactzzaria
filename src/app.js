6bc7fee937cc55d2b3d44442da055b4db82acb3d
Hiwoshu <hiwoshu@hotmail.com>
2019-03-27 19:28:12 -0300

    Refactoring on redirect rules

--- src/app.js
+++ src/app.js
@@ -1,11 +1,44 @@
-import React, { lazy, Suspense } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import React, { lazy, useEffect, useContext, useState, Suspense } from 'react'
+import t from 'prop-types'
+import { Redirect, Route, Switch } from 'react-router-dom'
 import { LinearProgress } from '@material-ui/core'
+import firebase from 'services/firebase'
+import { AuthContext } from 'contexts/auth'
 
 const MainPage = lazy(() => import('pages/main'))
 const Login = lazy(() => import('pages/login'))
 
-function App () {
+function App ({ location }) {
+  const { userInfo, setUserInfo, logout } = useContext(AuthContext)
+  const [didCheckUseIn, setDidCheckUseIn] = useState(false)
+
+  const { isUserLoggedIn } = userInfo
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      console.log('dados do usuário:', user)
+      setUserInfo({
+        isUserLoggedIn: !!user,
+        user
+      })
+      setDidCheckUseIn(true)
+    })
+
+    window.logout = logout
+  }, [])
+
+  if (didCheckUseIn) {
+    return <LinearProgress />
+  }
+
+  if (isUserLoggedIn && location.pathname === '/login') {
+    return <Redirect to='/' />
+  }
+
+  if (!isUserLoggedIn && location.pathname !== '/login') {
+    return <Redirect to='/login' />
+  }
+
   return (
     <Suspense fallback={<LinearProgress />}>
       <Switch>
@@ -14,6 +47,10 @@ function App () {
       </Switch>
     </Suspense>
   )
+}
+
+App.propTypes = {
+  location: t.object.isRequired
 }
 
 export default App
